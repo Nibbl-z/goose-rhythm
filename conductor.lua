0d1af0f59f841314c03a19f9162088d91eb727c8
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-07 15:24:55 -0600

    conductor in its own script

---
+++ conductor.lua
@@ -0,0 +1,22 @@
+conductor = {}
+
+conductor.BPM = 128
+conductor.SongPositionInBeats = 0
+conductor.SongPosition = 0.0
+conductor.LastBeat = 0.0
+conductor.SecondsPerBeat = 0.0
+
+function conductor:Update(dt)
+    self.SecondsPerBeat = 60 / self.BPM
+
+    self.SongPosition = self.SongPosition + dt
+    
+    if self.SongPosition > self.LastBeat + self.SecondsPerBeat then
+        self.SongPositionInBeats = self.SongPositionInBeats + 1
+        self.LastBeat = self.LastBeat + self.SecondsPerBeat
+
+        if self.Metronome ~= nil then
+            self.Metronome()
+        end
+    end
+end
--- main.lua
+++ main.lua
@@ -1,15 +1,12 @@
-local bpm = 128
-local song = "/kk_intermission.ogg"
-local beat = 0
+require("conductor")
 
-local lastBeat = 0
-local time = 0.0
+local song = "/kk_intermission.ogg"
+
 local testDraw = true
 
 function love.load()
     loadedSong = love.audio.newSource(song, "stream")
     metronome = love.audio.newSource("/select.wav", "static")
-    timePerBeat = 60 / bpm
     
     loadedSong:play()
     loadedSong:setVolume(0.2)
@@ -17,22 +14,16 @@ function love.load()
 end
 
 function love.update(dt)
-    time = time + dt
-    
-    if time > lastBeat + timePerBeat then
-        metronome:play()
-        beat = beat + 1
-        lastBeat = lastBeat + timePerBeat
-        testDraw = not testDraw
-    end
+    conductor:Update(dt)
+end
+
+function conductor.Metronome()
+    testDraw = not testDraw
+    metronome:play()
 end
 
 function love.draw()
-    --love.graphics.print(time)
-    love.graphics.print(beat, 0, 0)
-
     if testDraw then
         love.graphics.rectangle("fill", 100, 100, 100, 100)
     end
-    
 end
