1ef8d07419888d87238cea2ccf8eeab5d5867d6d
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-19 16:51:40 -0600

    THE white goose :D

---
+++ charts/whitegoose/metadata.lua
@@ -0,0 +1,11 @@
+return {
+    SongName = "White Goose",
+    SongArtist = "Nibbles",
+    Charter = "Nibbles",
+    BPM = 128,
+    GooseSize = 2,
+    DialogueTryAgain = "I am very upset with your performance... GRR!",
+    DialogueOK = "Good enough.",
+    DialogueSuperb = "WOWZERS!!! you really know how to uhh.... rhythm..",
+    DialoguePerfect = "ZOMG!"
+}
