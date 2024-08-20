098c80ff54b16ad5e846ecc8e196107f442e0b03
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-08-20 15:50:06 -0600

    other blue goose sprites

---
+++ charts/bluegoose/metadata.lua
@@ -0,0 +1,11 @@
+return {
+    SongName = "Blue Goose",
+    SongArtist = "Nibbles",
+    Charter = "Nibbles",
+    BPM = 150,
+    GooseSize = 2,
+    DialogueTryAgain = "Why would you let this happen!! :(",
+    DialogueOK = "Eh... good enough",
+    DialogueSuperb = "Wait, you did good?",
+    DialoguePerfect = "WHAT THE GOOSE!"
+}
